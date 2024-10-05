6b16c8bc5229b7218d2d68ce3811aab0bb4fc8d4
ZecroZhang <68031710+ZecroZhang@users.noreply.github.com>
2024-10-05 17:38:20 -0400

    feat: added projection math

--- webpage/internals.ts
+++ webpage/internals.ts
@@ -3,9 +3,22 @@ import { Coordinate } from "./types"
 export interface FOV {
     latitude: number
     longitude: number
-    fovDeg: number
+    fovReg: number
 }
 
 export interface Exoplanet {
     position: Coordinate
 }
+
+export const fov: FOV = {
+    latitude: 0,
+    longitude: 0,
+    fovReg: 10
+}
+
+export interface StarData {
+    position: Coordinate
+}
+
+
+export const stars: StarData[] = []
--- webpage/types.ts
+++ webpage/types.ts
@@ -1,1 +1,8 @@
 export type Coordinate = [number, number, number]
+
+export interface CelestialScreenObject {
+    x: number
+    y: number
+    radius: number
+    // brightness: number
+}
